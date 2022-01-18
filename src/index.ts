0b210d00edb040fd4f915b0806f5577752217f27
chei7tain <fredrickokwuobi@gmail.com>
2022-01-18 16:03:38 +0100

    add chalk package for coloring the terminal string

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,16 @@
-import {ApolloServer} from "apollo-server";
+import { ApolloServer } from "apollo-server";
 
-import {schema} from "./schema";
+import chalk from "chalk";
+
+import { schema } from "./schema";
 
 export const server = new ApolloServer({
-  schema
-})
+  schema,
+});
+
+const port = 3000;
+
+server.listen({ port }).then((data) => {
+  console.log(data);
+  console.log(chalk.green(`🚀 Server ready at ${data.url}`));
+});
